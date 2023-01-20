64c69ecf515301b57df9762d10acdc04525a3edf
George Anton <geocodlea@yahoo.com>
2023-01-20 11:59:12 +0200

    add keyboard instead of form

--- game.js
+++ game.js
@@ -24,9 +24,9 @@ const imageEl = document.getElementById("image");
 const incorrectEl = document.getElementById("incorrect");
 const remainingEl = document.getElementById("remaining");
 const scoreEl = document.getElementById("score");
-const form = document.querySelector("form");
 const playAgainBtn = document.getElementById("playAgain");
 const hintBtn = document.getElementById("hint");
+const keyboard = document.getElementById("keyboard");
 
 playAgainBtn.addEventListener("click", function () {
   // Reset the game
@@ -34,14 +34,14 @@ playAgainBtn.addEventListener("click", function () {
   incorrectGuesses = "";
   remainingGuesses = 6;
   word = wordList[Math.floor(Math.random() * wordList.length)];
-  form.style.display = "block";
   playAgainBtn.style.display = "none";
   imageEl.src = images[0];
   wordEl.textContent = "";
   incorrectEl.textContent = "Incorrect guesses: ";
   remainingEl.textContent = "Remaining guesses: 6";
   scoreEl.textContent = "Score: " + score;
-  document.getElementById("guess").value = "";
+  hintBtn.style.display = "block";
+  keyboard.style.display = "block";
 });
 
 hintBtn.addEventListener("click", function () {
@@ -52,57 +52,59 @@ hintBtn.addEventListener("click", function () {
   alert("Hint: " + hint);
 });
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  // Get the guess from the input field
-  const guess = document.getElementById("guess").value.toLowerCase();
+keyboard.addEventListener("click", function (e) {
+  if (e.target.matches(".key")) {
+    let guess = e.target.textContent.toLowerCase();
 
-  // Check if the guess is correct
-  if (word.includes(guess)) {
-    // Update the correct guesses array
-    for (let i = 0; i < word.length; i++) {
-      if (word[i] === guess && !correctGuesses.includes(guess)) {
-        correctGuesses.push(guess);
+    // Check if the guess is correct
+    if (word.includes(guess)) {
+      // Update the correct guesses array
+      for (let i = 0; i < word.length; i++) {
+        if (word[i] === guess && !correctGuesses.includes(guess)) {
+          correctGuesses.push(guess);
+        }
+      }
+    } else {
+      // Update the incorrect guesses string
+      if (!incorrectGuesses.includes(guess)) {
+        incorrectGuesses += guess;
+        remainingGuesses--;
       }
     }
-  } else {
-    // Update the incorrect guesses string
-    if (!incorrectGuesses.includes(guess)) {
-      incorrectGuesses += guess;
-      remainingGuesses--;
+
+    // Display the image
+    imageEl.src = images[6 - remainingGuesses];
+
+    // Display the word with the correct guesses filled in
+    let wordToShow = "";
+    for (let i = 0; i < word.length; i++) {
+      if (correctGuesses.includes(word[i])) {
+        wordToShow += word[i];
+      } else {
+        wordToShow += "_";
+      }
     }
-  }
+    wordEl.textContent = wordToShow;
+    incorrectEl.textContent = "Incorrect guesses: " + incorrectGuesses;
+    remainingEl.textContent = "Remaining guesses: " + remainingGuesses;
 
-  // Display the image
-  imageEl.src = images[6 - remainingGuesses];
-
-  // Display the word with the correct guesses filled in
-  let wordToShow = "";
-  for (let i = 0; i < word.length; i++) {
-    if (correctGuesses.includes(word[i])) {
-      wordToShow += word[i];
-    } else {
-      wordToShow += "_";
+    // Check if the player won or lost
+    if (word === wordToShow) {
+      score += remainingGuesses * 10;
+      scoreEl.innerHTML =
+        'You won! The word was: <b>"' + word + '"</b>. Your score is: ' + score;
+      playAgainBtn.style.display = "block";
+      hintBtn.style.display = "none";
+      keyboard.style.display = "none";
+    } else if (remainingGuesses === 0) {
+      scoreEl.innerHTML =
+        'You lost! The word was: <b>"' +
+        word +
+        '"</b>. Your score is: ' +
+        score;
+      playAgainBtn.style.display = "block";
+      hintBtn.style.display = "none";
+      keyboard.style.display = "none";
     }
-  }
-  wordEl.textContent = wordToShow;
-  incorrectEl.textContent = "Incorrect guesses: " + incorrectGuesses;
-  remainingEl.textContent = "Remaining guesses: " + remainingGuesses;
-
-  // Check if the player won or lost
-  if (word === wordToShow) {
-    score += remainingGuesses * 10;
-    alert("Congratulations! You discovered the word " + word);
-    scoreEl.textContent =
-      "You won! The word was " + word + ". Your score is: " + score;
-    form.style.display = "none";
-    playAgainBtn.style.display = "block";
-    hintBtn.style.display = "none";
-  } else if (remainingGuesses === 0) {
-    scoreEl.textContent =
-      "You lost! The word was " + word + ". Your score is: " + score;
-    form.style.display = "none";
-    playAgainBtn.style.display = "block";
-    hintBtn.style.display = "none";
   }
 });
